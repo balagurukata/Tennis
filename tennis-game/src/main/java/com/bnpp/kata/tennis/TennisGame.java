9a2a2b37b91d09e3af9521ae7cb05b1f3d8bbaf7
bala <gnagurus@gmail.com>
2020-07-27 23:53:30 +0530

    Verified: the game score point for first player is zero when game begins.

---
+++ tennis-game/src/main/java/com/bnpp/kata/tennis/TennisGame.java
@@ -0,0 +1,9 @@
+package com.bnpp.kata.tennis;
+
+public class TennisGame {
+
+	public Object getFirstPlayerScoredPoint() {
+		return 0;
+	}
+
+}
---
+++ tennis-game/src/test/java/com/bnpp/kata/tennis/TennisGameTest.java
@@ -0,0 +1,14 @@
+package com.bnpp.kata.tennis;
+
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+
+public class TennisGameTest {
+	@Test
+	public void firstPlayerScoredPointShouldBeEqualToZeroWhenTheGameStarts() {
+		TennisGame tennisGame = new TennisGame();
+		assertThat(0, is(tennisGame.getFirstPlayerScoredPoint()));
+	}
+}
